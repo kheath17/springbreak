76b1ef99665b2998f9c0cfdc7af277051e64d3f0
kheath17 <kheath17@vt.edu>
2017-03-14 10:31:05 -0400

    trouble steps

--- out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
+++
@@ -1,8 +0,0 @@
-//List of files which are indexed.
-fl = new Array();
-fl["0"]= "concepts/c-intro.html";
-fl["1"]= "tasks/t-beach.html";
-fl["2"]= "tasks/t-drive.html";
-fl["3"]= "tasks/t-house.html";
-fl["4"]= "tasks/t-pack.html";
-var doStem = false;searchLoaded = true;
